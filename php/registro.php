3f0a7a58fa26f56d2bf5f6665f61d1746bdaeaa8
nahuel de bellis <debellisnahuel@gmail.com>
2017-09-22 00:33:00 -0300

    Update registro.php

--- php/registro.php
+++ php/registro.php
@@ -3,7 +3,7 @@ header("Content-Type: application/json; charset=UTF-8");//por post
 $name = json_decode($_POST["h"], true);//or die("MUERTE");//Por post
 print_r($name);//verificar que no sea script js //XSS
 $i = 2;
-$reg = 'INSERT INTO `test`(`id`, `nombre`, `apellido`, `contra`) VALUES ( 210, "'.$name["nombre"].'", "lala21l", "'.$name["password"].'");';
+$reg = 'INSERT INTO `test`(`id`, `nombre`, `apellido`, `contra`, `desc`) VALUES ( 210, "'.$name["nombre"].'", "lala21l", "'.$name["Password"].', "'.$name["descripcion"].'");';#ATENCIO:FALTA MODIFICAR BASE DE DATOS, AGREGAR DESCRIPCION!!!
 $user = "root";
 $server = "localhost";
 $conexion = @mysql_connect($server, $user) #deprecated
@@ -11,3 +11,4 @@ or die("error");
 $db = mysql_select_db("test");
 $resul = mysql_query($reg);
 ?>
+
