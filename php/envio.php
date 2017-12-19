efa6cf1d02729a24a9a13b7e1cb715f94fbf75f8
nahuel de bellis <debellisnahuel@gmail.com>
2017-12-19 04:33:34 -0300

    Update envio.php

--- php/envio.php
+++ php/envio.php
@@ -11,27 +11,30 @@
 		$db = mysql_select_db("test");
 		$resul = mysql_query($sql);
 		$fila = mysql_fetch_row($resul) or die ("Password or Name incorrect");
-		echo "Connect";
+		echo "indexx.html";
 	}
 
-	else
-	{
-		echo "ERROR";
-	}
 	
-	echo "<br>lo recibido por post: <br>";
-	print_r($name);
-	echo "<br>";
+	/* Muestra lo obtenido*/
+	//echo "<br>lo recibido por post: <br>";
+	//print_r($name);
+	//echo "<br>";
 	$f = fopen("prueba.json", 'w');
-
 	fwrite($f, json_encode($name));
 	fclose($f);
 	$str_datos = file_get_contents("prueba.json");
 	$data = json_decode($str_datos, true);
-	echo "dentro de prueba.json: <br>";
-	print_r($data);
-	echo $name["nombre"];
-	
+	//echo "dentro de prueba.json: <br>";
+	//print_r($data);
+	//echo $name["nombre"];
+
+// Example by deerme.org
+/*$data = file_get_contents("https://tiendamovistar.terra.com/capa/cl/default.aspx");
+
+if ( preg_match('|<a href="http://mobile.terra.com/sc/cl/musicamagenta/">(.*?)</a>|is' , $data , $cap ) )
+{
+	echo "UF ".$cap[0];
+}*/
 	//$q = $_REQUEST["q"]; // Por GET
 ?>
 
